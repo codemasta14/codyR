55d324e1d92569fbbf3cea40de9539e03c5f36b6
Cody <codemasta14@gmail.com>
2018-11-13 15:52:52 -0700

    fixed bugs in res_pairs so that it can take interactions in the lm models

--- R/res_pairs.R
+++ R/res_pairs.R
@@ -4,20 +4,17 @@ res_pairs <- function(my_lm){
   variables<- deparse(my_lm$call)%>%
     str_extract("~.*data =")%>%
     str_remove_all("~|, data =")%>%
-    str_split("[+-\\:]")%>%
+    {if(str_detect(.,"I(.*)")){str_remove(.,"I\\(")%>%
+        str_remove("\\^\\d?.*\\)")}}%>%
+    str_split("[\\+\\-\\:]")%>%
     unlist%>%
-    str_trim()
-
-
+    str_trim()%>%
+    unique()
   og_df = get(df)
   filtered = og_df
   for(i in seq_along(1:length(variables))){
     filtered <- filter(filtered,!is.na(get(variables[i])))
-
   }
-
   binded = cbind(my_lm$residuals,filtered)
   pairs(right_join(binded,og_df),panel=panel.smooth)
-
 }
-
