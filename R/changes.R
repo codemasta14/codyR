e25c5178b25bda571e708e4ebaa11b2453659336
codemasta14 <codemasta14@gmail.com>
2019-02-05 13:21:55 -0700

    made changes function

--- R/changes.R
+++ R/changes.R
@@ -1,15 +1,25 @@
-my_mutate <- function(df, expr) {
-  expr <- enquo(expr)
-  change_name <- paste0("changed_", quo_name(expr))
-  up_name <- paste0("up_", quo_name(expr))
-  down_name <- paste0("down_", quo_name(expr))
+
+create_changes = function(df, expr) {
+  expr = enquo(expr)
+  change_name = paste0("changed_", quo_name(expr))
+  up_name = paste0("up_", quo_name(expr))
+  down_name = paste0("down_", quo_name(expr))
+  change = sym(change_name)
 
   mutate(df,
          !!change_name := case_when(
-           lag(!!expr)==!!expr ~T,
+           lag(!!expr)!=!!expr ~T,
            T ~ F
-         )
-  )
+         ),
+         !!up_name := case_when(
+           !!change & lag(!!expr)<!!expr ~T,
+           T ~ F
+         ),
+         !!down_name := case_when(
+           !!change & lag(!!expr)>!!expr ~T,
+           T ~ F
+         ))
+
+
 }
 
-my_mutate(faithful,waiting)
--- R/plus_model.R
+++ R/plus_model.R
@@ -1,29 +1,27 @@
 #incomplete
-plus_model <- function(my_lm,...){
-  df <<- get(str_remove_all(str_extract(str_flatten(deparse(my_lm$call)),"data = .*"),"data = |\\)"))
+#plus_model <- function(my_lm,...){
+#  df <<- get(str_remove_all(str_extract(str_flatten(deparse(my_lm$call)),"data = .*"),"data = |\\)"))
 
-      <- enquos(...)
-       early <<- deparse(my_lm$call)%>%
-         str_flatten()%>%
-        str_remove_all("lm|\\(|\\)")%>%
-         str_replace(",",paste0(
-           str_remove(as.character(more),pattern="~"),","))%>%
-        str_split(",")%>%
-        unlist()%>%
-        .[1]%>%
-        list()
+#      wow <-  enquos(...)
+#       early <<- deparse(my_lm$call)%>%
+#         str_flatten()%>%
+#        str_remove_all("lm|\\(|\\)")%>%
+#         str_replace(",",paste0(
+#           str_remove(as.character(more),pattern="~"),","))%>%
+#        str_split(",")%>%
+#        unlist()%>%
+#        .[1]%>%
+#        list()
 
        # sr_replace_all(pattern="\\s\\w",paste0(df,"$"))
 
 
-}
-plus_model(my_lm,+cyl)
-my_lm <- lm(mpg~wt,mtcars)
+#}
+#plus_model(my_lm,+cyl)
+#my_lm <- lm(mpg~wt,mtcars)
 
 
-?do.call
+#?do.call
 
-do.call("lm",plus_model(my_lm,+cyl))
+#do.call("lm",plus_model(my_lm,+cyl))
 
-
-cody,inc
